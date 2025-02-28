e15e12a7945fc05bd0361ca6c03eb936b6d53dab
Faysal2123 <foysal2005n1@gmail.com>
2025-03-01 00:58:48 +0600

    added footer section

--- src/MainLayouts/Layouts.jsx
+++ src/MainLayouts/Layouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Layouts = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div className='min-h-[calc(100vh-)]'><Outlet></Outlet></div>
+            <div className='min-h-[calc(100vh-300px)]'><Outlet></Outlet></div>
+            <Footer></Footer>
         </div>
     );
 };
